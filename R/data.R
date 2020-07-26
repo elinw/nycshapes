2fdd22928d505abc17cf6f12ec1c07c0148b4572
Elin Waring <elin.waring@gmail.com>
2020-07-26 00:05:14 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+
+# uhf to zip code
+#' @source  https://www.health.ny.gov/statistics/cancer/registry/appendix/neighborhoods.htm
+#'
+#' PUMAS to community districts reference
+#' https://www1.nyc.gov/assets/planning/download/pdf/data-maps/nyc-population/census2010/puma_cd_map.pdf
+#' https://www1.nyc.gov/site/planning/data-maps/nyc-population/geographic-reference.page
+#' PUMAS shapefile https://data.cityofnewyork.us/Housing-Development/Public-Use-Microdata-Areas-PUMA-/cwiz-gcty
+#'
+#' NTA shapefile
+#' https://data.cityofnewyork.us/City-Government/NTA-map/d3qk-pfyz
+#' Precincts
+#' https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz
+#' https://www.cccnewyork.org/wp-content/uploads/2019/11/NYC-Geographies.pdf
+#'
